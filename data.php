1b450566c320e13b9f2b3383556906866e30e9d7
leon <leonsegal1@gmail.com>
2017-05-01 12:30:31 +0100

    add data file

---
+++ data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: leon
+ * Date: 01/05/17
+ */
+
+// recent readings from EA station near maidenhead bridge as json (15 minutes = one period/reading, 288 readings is last few days):
+$url =  'http://environment.data.gov.uk/flood-monitoring/id/measures/2604TH-flow--i-15_min-m3_s/readings?_sorted&_limit=288';
+// parse json
+
+// use stored data to populate formula for last 3 days to predict gates status
