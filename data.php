655593549a47b708932730ae04a108d89ac06434
leon <leonsegal1@gmail.com>
2017-05-01 12:52:18 +0100

    add json decode

--- data.php
+++ data.php
@@ -5,7 +5,12 @@
  */
 
 // recent readings from EA station near maidenhead bridge as json (15 minutes = one period/reading, 288 readings is last few days):
-$url =  'http://environment.data.gov.uk/flood-monitoring/id/measures/2604TH-flow--i-15_min-m3_s/readings?_sorted&_limit=288';
+$url  = 'http://environment.data.gov.uk/flood-monitoring/id/measures/2604TH-flow--i-15_min-m3_s/readings?_sorted&_limit=288';
+$json = file_get_contents($url);
+
+
 // parse json
+$raw_data = json_decode($json);
+var_dump($raw_data);
 
 // use stored data to populate formula for last 3 days to predict gates status
